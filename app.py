2dfad903d2857a990e6b87a54eb9433ac6a8af39
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2020-09-03 22:19:43 +0000

    GitPod linked successfully with MongoDB collection

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'Milestone_three'
+app.config["MONGO_URI"] = 'mongodb+srv://root:Dunedin100@myfirstcluster.jekwe.mongodb.net/Milestone_three?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'hello world'
+@app.route('/get_countries')
+def get_countries():
+    return render_template("countries.html", reviews=mongo.db.reviews.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
