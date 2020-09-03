773fa4dc56fa3a5bdaec7f1ee04b97d87b4e2325
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2020-09-03 20:27:48 +0000

    added procfile

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
