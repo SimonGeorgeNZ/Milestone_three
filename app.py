aa0f0424f7c0d17deae1b90ceb0d731d9b7b2e6d
SimonGeorgeNZ <simon.george@stjohn.org.nz>
2020-09-06 22:39:41 +0000

    add countries function working correctly

--- app.py
+++ app.py
@@ -13,14 +13,29 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
+@app.route('/home')
 def buttons():
     return render_template('buttons.html')
+
+#--------------Countries-----------------#
 
 @app.route('/get_countries')
 def get_countries():
     return render_template('countries.html',
                            countries=mongo.db.countries.find())
 
+@app.route('/add_country')
+def add_country():
+    return render_template('addcountry.html')
+
+@app.route('/insert_country', methods=['POST'])
+def insert_country():
+    category_doc = {'country_name': request.form.get('country_name')}
+    mongo.db.countries.insert_one(category_doc)
+    return redirect(url_for('buttons'))
+
+
+#---------------Cities-----------------#
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
